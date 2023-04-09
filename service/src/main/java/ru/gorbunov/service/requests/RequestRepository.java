6384d5f1389e0fd77ede3a647c53c01bf43a95fd
Mikhail Gorbunov <mixalich84@yandex.ru>
2023-04-09 18:03:04 +0300

    feat: Added chek module(client, controller and service)

--- service/src/main/java/ru/gorbunov/service/requests/RequestRepository.java
+++ service/src/main/java/ru/gorbunov/service/requests/RequestRepository.java
@@ -11,6 +11,6 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
     List<Request> getAllRequestsWithStatusFromSize(String status, Integer from, Integer size);
 
 
-    @Query(value = "SELECT * FROM resources offset ? LIMIT ?", nativeQuery = true)
+    @Query(value = "SELECT * FROM requests offset ? LIMIT ?", nativeQuery = true)
     List<Request> getAllRequestsFromSize(Integer from, Integer size);
 }
